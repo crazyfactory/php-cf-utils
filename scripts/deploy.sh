c8127e51667d188f74210c4de22a659908bb644b
Jens Krause <jens@crazy-factory.com>
2017-06-07 15:26:43 +0700

    use updated deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,6 @@
 if [ -n $TRAVIS_TAG ]
 then
     printf "machine git.fury.io\nlogin $GEMFURY_USER\npassword $GEMFURY_PASSWORD" > ~/.netrc
-    git remote add fury https://git.fury.io/crazyfactory/php-jobs.git
+    git remote add fury "https://git.fury.io/$TRAVIS_REPO_SLUG.git"
     git push --tags fury HEAD:master
-else
-    echo "No tag found"
 fi
